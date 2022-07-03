938e7811b14861a5a039172a8feea3bbffd0e858
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-07-03 14:48:41 +0530

    adding date and location to the form and eventitem

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -9,14 +9,18 @@ const EventItem = (props) => {
     <div className = "col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{event.title}</h5>
+          <h5 className="card-title fw-semibold">{event.title}</h5>
           <p className="card-text">
             {event.description}
           </p> 
-          <button className = "mx-2" onClick = {()=>{deleteEvent(event.id)}}>d</button>
-          <button className = "mx-2">e</button>  
-          {/* <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}} ></i> */}
-          {/* <i className="fa-solid fa-pen-to-square mx-2"></i> */}
+          <p className="card-text text-success">
+            Event Date: {event.date}
+          </p> 
+          <p className="card-text text-warning text-secondary">
+            {event.location}
+          </p> 
+          <i className="fa-solid fa-trash mx-3" onClick = {()=>{deleteEvent(event.id)}}></i>
+          <i className="fa-solid fa-pen-to-square mx-2"></i>
         </div>
       </div>
     </div>
